b6ea82a2c853c4749b24ec0e3e6a15992bc3917e
niaj-a <niajahmd19@gmail.com>
2026-04-09 01:18:39 +0300

    Pushed from Android Studio

--- app/src/main/java/com/example/databasewithcomponents/MyDatabase.java
+++ app/src/main/java/com/example/databasewithcomponents/MyDatabase.java
@@ -8,29 +8,40 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class MyDatabase extends SQLiteOpenHelper {
-    public MyDatabase(@Nullable Context context, @Nullable String name, @Nullable
-    SQLiteDatabase.CursorFactory factory, int version) {
+
+    public MyDatabase(@Nullable Context context, @Nullable String name,
+                      @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE COMPONENTDATABASE(Name VARCHAR(255), RollNo VARCHAR(255), PhoneNo VARCHAR(255)," +
-                "Section VARCHAR(255), Course VARCHAR(255), Spinner VARCHAR(255), RadioBtn VARCHAR(255), CheckBox VARCHAR(255) )");
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE IF NOT EXISTS COMPONENTDATABASE(" +
+                        "Name VARCHAR(255), " +
+                        "RollNo VARCHAR(255), " +
+                        "PhoneNo VARCHAR(255), " +
+                        "Section VARCHAR(255), " +
+                        "Course VARCHAR(255), " +
+                        "Spinner VARCHAR(255), " +
+                        "RadioBtn VARCHAR(255), " +
+                        "CheckBox VARCHAR(255))"
+        );
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS COMPONENTDATABASE");
+        onCreate(sqLiteDatabase);
     }
 
-
-    public void insertValues(String name, String rollNo, String phoneNo, String section, String course, String spinner, String radioBtn, String checkBox) {
+    public void insertValues(String name, String rollNo, String phoneNo, String section,
+                             String course, String spinner, String radioBtn, String checkBox) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "INSERT INTO COMPONENTDATABASE (Name, RollNo, PhoneNo, Section, Course, Spinner, RadioBtn, CheckBox) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
+        String query = "INSERT INTO COMPONENTDATABASE " +
+                "(Name, RollNo, PhoneNo, Section, Course, Spinner, RadioBtn, CheckBox) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         db.execSQL(query, new String[]{name, rollNo, phoneNo, section, course, spinner, radioBtn, checkBox});
-//        db.close();
     }
 
     public void deleteValues(String name) {
@@ -39,21 +50,39 @@ public class MyDatabase extends SQLiteOpenHelper {
     }
 
     public String displayData() {
-        String data = "";
+        StringBuilder data = new StringBuilder();
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM COMPONENTDATABASE", null);
-        while (cursor.moveToNext()) {
-            String a = cursor.getString(0);
-            String b = cursor.getString(1);
-            String c = cursor.getString(2);
-            String d = cursor.getString(3);
-            String e = cursor.getString(4);
-            String f = cursor.getString(5);
-            String g = cursor.getString(6);
-            String h = cursor.getString(7);
-            data += a + b + c + d + e + f + g + h;
+
+        try {
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(0);
+                String rollNo = cursor.getString(1);
+                String phoneNo = cursor.getString(2);
+                String section = cursor.getString(3);
+                String course = cursor.getString(4);
+                String spinner = cursor.getString(5);
+                String radioBtn = cursor.getString(6);
+                String checkBox = cursor.getString(7);
+
+                data.append("Name: ").append(name).append("\n")
+                        .append("RollNo: ").append(rollNo).append("\n")
+                        .append("PhoneNo: ").append(phoneNo).append("\n")
+                        .append("Section: ").append(section).append("\n")
+                        .append("Course: ").append(course).append("\n")
+                        .append("Spinner: ").append(spinner).append("\n")
+                        .append("RadioBtn: ").append(radioBtn).append("\n")
+                        .append("CheckBox: ").append(checkBox).append("\n\n");
+            }
+        } finally {
+            cursor.close();
         }
-        return data;
+
+        if (data.length() == 0) {
+            return "No data found";
+        }
+
+        return data.toString();
     }
 }
 
